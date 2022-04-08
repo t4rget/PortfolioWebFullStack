c1ed1070e763ae2a8af1cdf3a573a98a90a67455
t4rget <taron.jonaa@hotmail.com>
2022-04-08 10:49:56 -0300

    int bootstrap/load assets/create components

--- Front-End-Angular/protectofinal_t4rget/src/app/app.module.ts
+++ Front-End-Angular/protectofinal_t4rget/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent
   ],
   imports: [
     BrowserModule,
