2426030f7e823fe3248f9b0236fdcc153cc7ed73
t4rget <taron.jonaa@hotmail.com>
2022-05-22 01:14:17 -0300

    se definio PersonaService y se implemento la interca de IPersonaService

--- SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/services/PersonaService.java
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/services/PersonaService.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.t4rget.portfolio.services;
 
-/**
- *
- * @author jhon
- */
-public class PersonaService {
+import com.t4rget.portfolio.model.Persona;
+import com.t4rget.portfolio.repository.PersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService implements IPersonaService{
+
+    @Autowired
+    
+    public PersonaRepository persoRepo;
+    
+    @Override
+    public List<Persona> verPersonasc() {
+       return persoRepo.findAll();
+    }
+
+    @Override
+    public void crearPersonac(Persona per) {
+        persoRepo.save(per);
+    }
+
+    @Override
+    public void borrarPersona(Long id) {
+        persoRepo.deleteById(id);
+    }
+
+    @Override
+    public Persona buscarPersona(Long id) {
+        return persoRepo.findById(id).orElse(null);
+    }
     
 }
