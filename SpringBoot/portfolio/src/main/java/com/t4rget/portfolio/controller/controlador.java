c1ded62be5ac45822f86a8359c0dcdcd6c8a0b27
t4rget <taron.jonaa@hotmail.com>
2022-05-22 01:34:26 -0300

    se corrigio el entity en persona.java y se corrigio tmb controlador.java

--- SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/controller/controlador.java
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/controller/controlador.java
@@ -1,7 +1,15 @@
 package com.t4rget.portfolio.controller;
 
-import com.t4rget.portfolio.repository.PersonaRepository;
+import com.t4rget.portfolio.model.Persona;
+import com.t4rget.portfolio.services.IPersonaService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +17,23 @@ public class controlador {
     
     @Autowired
     
-    public PersonaRepository persoRepo;
+    public IPersonaService persoServ;
     
+    @PostMapping ("/new/persona")
+    public void agregarPersona (@RequestBody Persona pers) {
+        persoServ.crearPersonac(pers);
+    }
     
+    @GetMapping ("/ver/personas")
+    @ResponseBody
+    public List<Persona> verPersonas () {
+        return persoServ.verPersonasc();
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public void borrarPersona (@PathVariable Long id) {
+        persoServ.borrarPersona(id);
+    }
+
     
 }
