9b1d6669c4df47054d290864f0bf071eb99c347e
t4rget <taron.jonaa@hotmail.com>
2022-05-21 23:03:23 -0300

    se creo el directorio de la api

---
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/PortfolioArgentinaProgramaApplication.java
@@ -0,0 +1,13 @@
+package com.t4rget.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioArgentinaProgramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioArgentinaProgramaApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/portfolio/src/test/java/com/t4rget/portfolio/PortfolioArgentinaProgramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t4rget.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioArgentinaProgramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
