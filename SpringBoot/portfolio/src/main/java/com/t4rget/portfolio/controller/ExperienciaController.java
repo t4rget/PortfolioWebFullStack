703765f5a63032e12fcbd475b3909d8f8f1db30d
t4rget <taron.jonaa@hotmail.com>
2022-05-28 22:40:15 -0300

    se definio el controller de experiencia

--- SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/controller/ExperienciaController.java
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/controller/ExperienciaController.java
@@ -1,7 +1,61 @@
 
 package com.t4rget.portfolio.controller;
 
+import com.t4rget.portfolio.model.Experiencia;
+import com.t4rget.portfolio.service.ExperienciaService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/experiencia")
 public class ExperienciaController {
+    
+    private final ExperienciaService experienciaService;
+
+    public ExperienciaController(ExperienciaService experienciaService) {
+        this.experienciaService = experienciaService;
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Experiencia>> traerExperiencias() {
+        
+        List<Experiencia> experiencias=experienciaService.buscarExperiencias();
+        
+        return new ResponseEntity<>(experiencias, HttpStatus.OK);
+    }
+    
+    @PutMapping("/update")
+    public ResponseEntity<Experiencia> editarExperiencia(@RequestBody Experiencia experiencia){
+
+        Experiencia upExperiencia=experienciaService.editarExperiencia(experiencia);
+
+        return new ResponseEntity<>(upExperiencia,HttpStatus.OK);  
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Experiencia> agregarExperiencia(@RequestBody Experiencia experiencia){
+        
+        Experiencia agregarExperiencia=experienciaService.agregarExperiencia(experiencia);
+        
+        return new ResponseEntity<>(agregarExperiencia,HttpStatus.CREATED);
+    }  
+    
+    @DeleteMapping("delete/{id}")
+    public  ResponseEntity<?> borrarExperiencia(@PathVariable("id") Long id){
+        
+        experienciaService.borrarExperiencia(id);
+        
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
     
 }
