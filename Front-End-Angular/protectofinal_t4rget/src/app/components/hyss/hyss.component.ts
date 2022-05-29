81d4a8472e4d85fa911beb159a41e6f084fb39aa
t4rget <taron.jonaa@hotmail.com>
2022-05-29 00:46:58 -0300

    se replicaron los metodos del component de educacion en los component de experiencia, proyectos, aptitudes

--- Front-End-Angular/protectofinal_t4rget/src/app/components/hyss/hyss.component.ts
+++ Front-End-Angular/protectofinal_t4rget/src/app/components/hyss/hyss.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Aptitud } from 'src/app/model/aptitud';
+import { AptitudService } from 'src/app/service/aptitud.service';
 
 @Component({
   selector: 'app-hyss',
@@ -7,9 +11,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HyssComponent implements OnInit {
 
-  constructor() { }
+  public aptitudes:Aptitud[]=[];
+  public editAptitud: Aptitud | undefined;
+  public deleteAptitud: Aptitud | undefined;
+
+  constructor(private aptitudService : AptitudService) { }
 
   ngOnInit(): void {
+
+    this.getAptitud();
   }
+
+  public getAptitud(): void {
+    this.aptitudService.getAptitud().subscribe({
+      next: (Response: Aptitud[]) => {
+        this.aptitudes = Response;
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+  public onOpenModal(mode:String, aptitud?: Aptitud):void{
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.type="button";
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode==='add'){
+      button.setAttribute('data-target', '#addAptitudModal');
+    }else if(mode==='delete'){
+      this.deleteAptitud = aptitud;
+      button.setAttribute('data-target', '#deleteAptitudModal')
+    }else if(mode==='edit'){
+      this.editAptitud = aptitud;
+      button.setAttribute('data-target', '#editAptitudModal')
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+
+  public onAddAptitud(addForm: NgForm): void{
+    document.getElementById('add-aptitud-form')?.click();
+    this.aptitudService.addAptitud(addForm.value).subscribe({
+      next: (response:Aptitud) =>{
+        console.log(response);
+        this.getAptitud();
+        addForm.reset();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+        addForm.reset();
+      }
+    })
+  }
+
+
+  public onUpdateAptitud(aptitud: Aptitud){
+    this.editAptitud=aptitud;
+    document.getElementById('add-aptitud-form')?.click();
+    this.aptitudService.updateAptitud(aptitud).subscribe({
+      next: (response:Aptitud) =>{
+        console.log(response);
+        this.getAptitud();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+
+  public onDeleteAptitud(idEdu: number):void{
+    this.aptitudService.deleteAptitud(idEdu).subscribe({
+      next: (response:void) =>{
+        console.log(response);
+        this.getAptitud();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
 
 }
--- Front-End-Angular/protectofinal_t4rget/src/app/components/proyectos/proyectos.component.ts
+++ Front-End-Angular/protectofinal_t4rget/src/app/components/proyectos/proyectos.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Proyecto } from 'src/app/model/proyecto';
+import { ProyectoService } from 'src/app/service/proyecto.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,9 +11,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  public proyectos:Proyecto[]=[];
+  public editProyecto: Proyecto | undefined;
+  public deleteProyecto: Proyecto | undefined;
+
+  constructor(private proyectoService : ProyectoService) { }
 
   ngOnInit(): void {
+
+    this.getProyecto();
   }
+
+  public getProyecto(): void {
+    this.proyectoService.getProyecto().subscribe({
+      next: (Response: Proyecto[]) => {
+        this.proyectos = Response;
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+  public onOpenModal(mode:String, proyecto?: Proyecto):void{
+    const container=document.getElementById('main-container');
+    const button=document.createElement('button');
+    button.type="button";
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+    if(mode==='add'){
+      button.setAttribute('data-target', '#addProyectoModal');
+    }else if(mode==='delete'){
+      this.deleteProyecto = proyecto;
+      button.setAttribute('data-target', '#deleteProyectoModal')
+    }else if(mode==='edit'){
+      this.editProyecto = proyecto;
+      button.setAttribute('data-target', '#editProyectoModal')
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+
+  public onAddProyecto(addForm: NgForm): void{
+    document.getElementById('add-proyecto-form')?.click();
+    this.proyectoService.addProyecto(addForm.value).subscribe({
+      next: (response:Proyecto) =>{
+        console.log(response);
+        this.getProyecto();
+        addForm.reset();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+        addForm.reset();
+      }
+    })
+  }
+
+
+  public onUpdateProyecto(proyecto: Proyecto){
+    this.editProyecto=proyecto;
+    document.getElementById('add-proyecto-form')?.click();
+    this.proyectoService.updateProyecto(proyecto).subscribe({
+      next: (response:Proyecto) =>{
+        console.log(response);
+        this.getProyecto();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
+
+  public onDeleteProyecto(idEdu: number):void{
+    this.proyectoService.deleteProyecto(idEdu).subscribe({
+      next: (response:void) =>{
+        console.log(response);
+        this.getProyecto();
+      },
+      error: (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    })
+  }
+
 
 }
