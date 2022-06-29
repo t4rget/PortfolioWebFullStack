22e167dc2b3e61d980f1486504d66be45a8cd309
t4rget <taron.jonaa@hotmail.com>
2022-06-29 00:03:48 -0300

    se corrigio el interceptor en el front, y en la api se corrigio los guards de los path y los metodos

--- Front-End-Angular/protectofinal_t4rget/src/app/service/interceptor.service.ts
+++ Front-End-Angular/protectofinal_t4rget/src/app/service/interceptor.service.ts
@@ -11,17 +11,20 @@ export class InterceptorService implements HttpInterceptor {
   constructor(private loginService: LoginService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    var session = sessionStorage.getItem('currentUser') as unknown as datos;
+    var currentUser = this.loginService.UsuarioAutenticado;
+    //var session = sessionStorage.getItem('currentUser') as unknown as datos;
     //console.log("SESSION LOG", session);
-    if(session && session.accessToken)
+    //if(session && session.accessToken)
+    if(currentUser && currentUser.accessToken)
     {
       req=req.clone({
         setHeaders:{
-          Authorization: `Bearer ${session.accessToken}`
+          //Authorization: `Bearer ${session.accessToken}`
+          Authorization: `Bearer ${currentUser.accessToken}`
         }
       })
     }
-    //console.log("Interceptor esta corriendo" /*+ JSON.stringify(currentUser.accessToken)*/);
+    console.log("Interceptor esta corriendo" /*+ JSON.stringify(currentUser.accessToken)*/);
     return next.handle(req);
   }
 }
--- SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/SecurityConfig.java
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import com.t4rget.portfolio.repository.UsuarioRepo;
 import java.util.Arrays;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
@@ -64,18 +65,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         return  super.authenticationManager();
     }
     
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws  Exception {
-        http.csrf().disable()
-                /*
+        /*http.csrf().disable()
+                
                 .authorizeRequests().antMatchers("/api/login")
                     .permitAll()
                 .antMatchers("/api/**")
                 .authenticated()
                 .and()
                 .httpBasic();
-                */
                 
+                
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/api/login").permitAll()
+                .anyRequest().authenticated()
+    
+                /*
                 //old
                 .authorizeRequests().antMatchers("/api/**")
                 .permitAll()
@@ -84,7 +90,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
                 .httpBasic();
                 
-         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+                 .and().addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                
+        //http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }*/
      
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+         /*
+         * 1. Se desactiva el uso de cookies
+         * 2. Se activa la configuración CORS con los valores por defecto
+         * 3. Se desactiva el filtro CSRF
+         * 4. Se indica que el login no requiere autenticación
+         * 5. Se indica que el resto de URLs esten securizadas
+	*/
+	httpSecurity
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .cors().and()
+            .csrf().disable()
+            // SE PUEDE DEFINIR POR PATH
+            .authorizeRequests().antMatchers("/api/login").permitAll()
+            //.antMatchers("/api/persona/id/**").permitAll()
+            //.antMatchers("/api/educacion/").permitAll()
+            
+            // SE PUEDE DEFINIR POR METODO (EJ:GET,PUT,POST,DELETE)    
+            .antMatchers(HttpMethod.GET).permitAll()
+            .antMatchers(HttpMethod.PUT).authenticated()
+            .antMatchers(HttpMethod.POST).authenticated()
+            .antMatchers(HttpMethod.DELETE).authenticated()
+                .anyRequest().authenticated().and().httpBasic();
+                
+        
+         httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                //.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                //.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+	}
 } 
