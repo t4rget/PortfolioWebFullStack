0bbac39da83675210acb779f8422ad8e3a61814e
t4rget <taron.jonaa@hotmail.com>
2022-07-24 11:47:49 -0300

    push anterior al deploy, se corrigio parte de redes sociales

--- SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/tempDelete.java
+++ SpringBoot/portfolio/src/main/java/com/t4rget/portfolio/tempDelete.java
@@ -1,10 +1,12 @@
 /*
 
-spring.datasource.url=jdbc:mysql://u4xedst2g7dtksic:Kb4TXH5YpqkPgYGOpXGn@bdljgdhz1tixpqx6uvel-mysql.services.clever-cloud.com:3306/bdljgdhz1tixpqx6uvel
 spring.datasource.username=u4xedst2g7dtksic
 spring.datasource.password=Kb4TXH5YpqkPgYGOpXGn
+spring.datasource.url=jdbc:mysql://u4xedst2g7dtksic:Kb4TXH5YpqkPgYGOpXGn@bdljgdhz1tixpqx6uvel-mysql.services.clever-cloud.com:3306/bdljgdhz1tixpqx6uvel
 
-
+spring.datasource.username=root
+spring.datasource.password=2539
+spring.datasource.url=jdbc:mysql://localhost:3306/portfolioARGv2?useSSL=false&serverTimezone=UTC
 
 
 @Test
